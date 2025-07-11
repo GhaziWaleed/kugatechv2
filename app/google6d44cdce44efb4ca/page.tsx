717bc84a60bffe3cb41dcfb221e301fde4f3b79c
GhaziWaleed <ghaziwaleedasif@gmail.com>
2025-07-11 19:02:07 +0500

    sda

---
+++ app/google6d44cdce44efb4ca/page.tsx
@@ -0,0 +1,10 @@
+export const dynamic = "force-static"; // ensures static generation
+
+export default function GoogleVerificationPage() {
+  return (
+    <>
+      google-site-verification: google6d44cdce44efb4ca.html
+    </>
+  );
+}
+
